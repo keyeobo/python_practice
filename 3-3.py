ceedad9ddc977b9b84989fb0363a49540d3705c4
keyeobo <48973615+keyeobo@users.noreply.github.com>
2022-05-18 12:28:11 +0900

    이것이 코딩테스트다 예제 3-4 1이 될 때까지

--- 3-3.py
+++ 3-3.py
@@ -21,7 +21,7 @@ result = 0
 
 for i in range(n):
     data = list(map(int, input().split()))
-    min_value = min(data)
+    min_value = min(data) 
     result = max(result, min_value)
 
 print(result)
